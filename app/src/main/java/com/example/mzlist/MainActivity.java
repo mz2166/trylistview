e4f00b4425a468a0617a3005d778825d88292ea0
PC-11 <mz2820031@gmail.com>
2025-10-01 08:59:03 +0300

    final

--- app/src/main/java/com/example/mzlist/MainActivity.java
+++ app/src/main/java/com/example/mzlist/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 
@@ -21,13 +23,21 @@ public class MainActivity extends AppCompatActivity {
     ListView listViewFruits;
     ImageView imageViewFruit;
     ArrayList<String> fruitsarr = new ArrayList<>();
+    EditText editImageNumber, editImageName;
+    Button buttonAdd;
     int[] fruitImages = {
             R.drawable.apple,   // index 0 = Apple
             R.drawable.banana,  // index 1 = Banana
             R.drawable.orange,  // index 2 = Orange
             R.drawable.grape,   // index 3 = Grape
-            R.drawable.mango    // index 4 = Mango
+            R.drawable.mango,
+            R.drawable.carrot,    // 5
+            R.drawable.cherries,    // 6
+            R.drawable.watermelon,// index 4 = Mango
     };
+
+    ArrayList<Integer> fruitImageList = new ArrayList<>();
+    ArrayAdapter<String> adapter;
 
 
     @Override
@@ -53,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
                 imageViewFruit.setImageResource(fruitImages[position]);
             }
         });
+        //thats my xml and java i want when i put image fruit name and puts its id and press add it added it to the listview and put the image of the spesefice fruit the fruitts that i want to add is carrots and cherry and watermelon these fruits are just to add and i have previus fruit names and images that it just added to the listview and the number of the image is it index and when i put watermelon in image name edittext it shows watermelon image in imge section
 
 
     }
