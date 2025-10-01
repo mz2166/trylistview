2a0ebef958acb123abec076c9c8720983e3c4400
mz282 <mz2820031@gmail.com>
2025-10-01 23:08:42 +0300

    final

--- app/src/main/java/com/example/mzlist/MainActivity.java
+++ app/src/main/java/com/example/mzlist/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,52 +20,130 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    String[] fruits = {"Apple", "Banana", "Orange", "Grape", "Mango"};
+    String[] fruits = {"Apple", "Banana", "Orange", "Mango"};
     ListView listViewFruits;
     ImageView imageViewFruit;
     ArrayList<String> fruitsarr = new ArrayList<>();
     EditText editImageNumber, editImageName;
     Button buttonAdd;
     int[] fruitImages = {
-            R.drawable.apple,   // index 0 = Apple
-            R.drawable.banana,  // index 1 = Banana
-            R.drawable.orange,  // index 2 = Orange
-            R.drawable.grape,   // index 3 = Grape
+            R.drawable.apple,
+            R.drawable.banana,
+            R.drawable.orange,
             R.drawable.mango,
-            R.drawable.carrot,    // 5
-            R.drawable.cherries,    // 6
-            R.drawable.watermelon,// index 4 = Mango
+            R.drawable.carrot,
+            R.drawable.cherries,
+            R.drawable.watermelon,
     };
 
-    ArrayList<Integer> fruitImageList = new ArrayList<>();
+    ArrayList<Integer> imageIndexList = new ArrayList<>();
     ArrayAdapter<String> adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // your layout file here
+        setContentView(R.layout.activity_main);
 
         listViewFruits = findViewById(R.id.listViewFruits);
         imageViewFruit = findViewById(R.id.imageViewFruit);
+        editImageNumber = findViewById(R.id.editImageNumber);
+        editImageName = findViewById(R.id.editImageName);
+        buttonAdd = findViewById(R.id.buttonAdd);
 
-        // Convert array to ArrayList
         fruitsarr = new ArrayList<>(Arrays.asList(fruits));
 
-        // Set adapter for ListView
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
-                android.R.layout.simple_list_item_1,
-                fruitsarr);
+
+        imageIndexList = new ArrayList<>();
+        for (int i = 0; i < fruits.length; i++) {
+            imageIndexList.add(i);
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, fruitsarr);
         listViewFruits.setAdapter(adapter);
+
+
         listViewFruits.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                // Use position to get image from fruitImages array
-                imageViewFruit.setImageResource(fruitImages[position]);
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                int imageIndex = imageIndexList.get(position);
+                if (imageIndex >= 0 && imageIndex < fruitImages.length) {
+                    imageViewFruit.setImageResource(fruitImages[imageIndex]);
+                }
             }
         });
-        //thats my xml and java i want when i put image fruit name and puts its id and press add it added it to the listview and put the image of the spesefice fruit the fruitts that i want to add is carrots and cherry and watermelon these fruits are just to add and i have previus fruit names and images that it just added to the listview and the number of the image is it index and when i put watermelon in image name edittext it shows watermelon image in imge section
 
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editImageName.getText().toString().trim();
+
+                if (name.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please enter a fruit name", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                int imageIndex = -1;
+                if (name.equalsIgnoreCase("Carrot")) {
+                    imageIndex = 4;
+                } else if (name.equalsIgnoreCase("Cherries")) {
+                    imageIndex = 5;
+                } else if (name.equalsIgnoreCase("Watermelon")) {
+                    imageIndex = 6;
+                } else {
+                    Toast.makeText(MainActivity.this, "You can only add Carrot, Cherries, or Watermelon", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+                fruitsarr.add(name);
+                imageIndexList.add(imageIndex);
+                adapter.notifyDataSetChanged();
+
+                editImageName.setText("");
+                Toast.makeText(MainActivity.this, name + " added!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        listViewFruits.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                int imageIndex = imageIndexList.get(position);
+
+                if (imageIndex >= 0 && imageIndex < fruitImages.length) {
+
+                    imageViewFruit.setImageResource(fruitImages[imageIndex]);
+
+
+                    Toast.makeText(MainActivity.this,
+                            "The id of the fruit is " + imageIndex,
+                            Toast.LENGTH_SHORT).show();
+
+
+                    editImageNumber.setText(String.valueOf(imageIndex));
+                }
+            }
+        });
+
+
+        listViewFruits.setOnItemLongClickListener((adapterView, view, position, id) -> {
+            String fruitName = fruitsarr.get(position);
+
+            new android.app.AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Delete Fruit")
+                    .setMessage("Are you sure you want to delete \"" + fruitName + "\"?")
+                    .setPositiveButton("Yes", (dialog, which) -> {
+                        fruitsarr.remove(position);
+                        imageIndexList.remove(position);
+                        adapter.notifyDataSetChanged();
+                        Toast.makeText(MainActivity.this, fruitName + " deleted", Toast.LENGTH_SHORT).show();
+                    })
+                    .setNegativeButton("No", null)
+                    .show();
+
+            return true;
+        });
 
     }
 }
